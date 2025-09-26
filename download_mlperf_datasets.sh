f10265401649d01d0030a33d4b6331f34c96a108
naveenmiriyaluredhat <nmiriyal@redhat.com>
2025-09-26 11:48:59 -0400

    Script to download datasets

---
+++ download_mlperf_datasets.sh
@@ -0,0 +1,13 @@
+DATASET_HOME=/mnt/datasets
+
+bash <(curl -s https://raw.githubusercontent.com/mlcommons/r2-downloader/refs/heads/main/mlc-r2-downloader.sh) \
+  -d ${DATASET_HOME}/llama3.1-8b \
+  https://inference.mlcommons-storage.org/metadata/llama3-1-8b-cnn-eval.uri
+
+bash <(curl -s https://raw.githubusercontent.com/mlcommons/r2-downloader/refs/heads/main/mlc-r2-downloader.sh) \
+  -d ${DATASET_HOME}/deepseek-r1 \
+  https://inference.mlcommons-storage.org/metadata/deepseek-r1-datasets-fp8-eval.uri
+
+bash <(curl -s https://raw.githubusercontent.com/mlcommons/r2-downloader/refs/heads/main/mlc-r2-downloader.sh) \
+  -d ${DATASET_HOME}/whisper  \
+  https://inference.mlcommons-storage.org/metadata/whisper-model.uri
